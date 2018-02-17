5a689d0e15ffbd8c8133e56b0f6f62945b645ac4
Aaron Saunders <aaron@clearlyinnovative.com>
2018-02-17 16:14:29 -0500

    more styling updates

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,63 +1,62 @@
-<template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar class="toolbar-md-primary">
-        <ion-title>TodoList</ion-title>
-      </ion-toolbar>
-		</ion-header>
-		<ion-content class="content" padding>
+<template >
+<ion-page>
+  <ion-header>
+    <ion-toolbar class="toolbar-md-primary">
+      <ion-title>TodoList</ion-title>
+    </ion-toolbar>
+  </ion-header>
+  <ion-content class="content" padding>
 
-<ion-card>
-		<ion-item>
-			<ion-label>Fave Type</ion-label>
-			<select v-model="fav.type">
-        <option disabled value="">Please select one</option>
-        <option value="shared">shared</option>
-        <option value="personal">personal</option>
-        <option value="work">work</option>
-			</select>
-		</ion-item>
-		<ion-item>
-			<ion-label>Fave Name</ion-label>
-			<ion-input type="text" placeholder="name to associate with link" 
-        :value="fav.name"  
-        v-on:input="fav.name = $event.target.value">
-      </ion-input>
-		</ion-item>
-		<ion-item>
-			<ion-label>Fave Link</ion-label>
-			<ion-input type="text" placeholder="website url" 
-        :value="fav.link"  
-        v-on:input="fav.link = $event.target.value">
-      </ion-input>
-		</ion-item>
-    <div padding>
-		  <ion-button @click="addFavorite()">ADD FAVORITE</ion-button>
-    </div>
-</ion-card>
+    <ion-card>
       <ion-item>
-        <ion-label><strong>Filter On Favorite Type</strong></ion-label>
-        <select @change="selectedFilter($event)">
+        <ion-label>Fave Type</ion-label>
+        <select v-model="fav.type">
           <option disabled value="">Please select one</option>
-          <option value="all">show all</option>
           <option value="shared">shared</option>
           <option value="personal">personal</option>
           <option value="work">work</option>
         </select>
-      </ion-item>      
-      <ion-list>
+      </ion-item>
+      <ion-item>
+        <ion-label>Fave Name</ion-label>
+        <ion-input type="text" placeholder="name to associate with link" :value="fav.name" v-on:input="fav.name = $event.target.value">
+        </ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>Fave Link</ion-label>
+        <ion-input type="text" placeholder="website url" :value="fav.link" v-on:input="fav.link = $event.target.value">
+        </ion-input>
+      </ion-item>
+      <div padding>
+        <ion-button @click="addFavorite()">ADD FAVORITE</ion-button>
+      </div>
+    </ion-card>
+    <ion-item>
+      <ion-label>
+        <strong>Filter On Favorite Type</strong>
+      </ion-label>
+      <select @change="selectedFilter($event)">
+        <option disabled value="">Please select one</option>
+        <option value="all">show all</option>
+        <option value="shared">shared</option>
+        <option value="personal">personal</option>
+        <option value="work">work</option>
+      </select>
+    </ion-item>
+    <ion-list>
       <ion-item v-for="f in favorites" :key="f.id">
         <ion-label full style="color:black" @click="showDetail(f.id)">
           [ {{f.type}} ] {{f.name}} - {{f.link}}</ion-label>
         <button ion-button @click="removeFromFavorites(f.id)">DELETE</button>
       </ion-item>
-      </ion-list>
-			<!-- <ion-fab-button  @click="addFavorite">
+    </ion-list>
+    <!-- <ion-fab-button  @click="addFavorite">
         <ion-icon name="add"></ion-icon>
       </ion-fab-button> -->
 
-		</ion-content>
-	</ion-page>
+  </ion-content>
+</ion-page>
+
 
 </template>
 
@@ -90,7 +89,7 @@ export default {
     },
     addFavorite() {
       this.$store.dispatch('addFavorite', this.fav);
-      this.fav = {}
+      this.fav = {};
     },
     showDetail(_id) {
       this.$router.push({ path: '/' + _id });
